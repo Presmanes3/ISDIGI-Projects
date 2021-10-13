126ccfe9a69a9a0c1413055757483c80c9c8632e
Iván <ivivpas@teleco.upv.es>
2021-10-13 16:51:14 +0200

    First version of the files

---
+++ Practica 2 - Sum and Shift Based Multiplier/testbench/scoreboard.sv
@@ -0,0 +1,13 @@
+class scoreboard;
+
+system_iff sys_iff;
+
+
+task multiply(reg A,reg B);
+    
+endtask
+
+task compare_outputs(reg ideal_result, reg real_result);
+
+endtask
+endclass
---
+++ Practica 2 - Sum and Shift Based Multiplier/testbench/testbench.sv
@@ -0,0 +1,24 @@
+module testbench(
+    
+);
+
+// Instanciate the common system interface for signals
+system_iff sys_iff;
+
+// Instanciate scoreboard
+scoreboard scoreboard_;
+
+// Instanciate multipli control and link system interface
+multipli_control multi_control_module(
+    .sys_iff(sys_iff)
+);
+
+initial begin
+    scoreboard_ = new();
+
+    // Link system interface
+    scoreboard_.sys_iff = sys_iff;
+end
+
+
+endmodule
