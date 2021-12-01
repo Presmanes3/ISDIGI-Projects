865ea1edcbc55f7884a51c124a50873ae82895c2
Javier <presmanes3@gmail.com>
2021-12-01 12:58:24 +0100

    forced register 0 to 0

--- Practica 3 - RISC V/Core/Design/register_bank.sv
+++ Practica 3 - RISC V/Core/Design/register_bank.sv
@@ -14,6 +14,8 @@ module register_bank (
 reg [31:0] reg_pool [31:0];
 
 always_ff @( posedge clk ) begin 
+    reg_pool[0] = 32'd0;
+    
     if(write_enable)begin
         if(write_register_addr != 5'b00000)begin
             reg_pool[write_register_addr] <= write_data;
