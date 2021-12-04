015fa37f3416e68fa1cc72cf10d805f9533b6bf7
Javier <presmanes3@gmail.com>
2021-12-04 17:11:52 +0100

    Solved BUG!

--- Practica 3 - RISC V/Core/Design/immgen.sv
+++ Practica 3 - RISC V/Core/Design/immgen.sv
@@ -4,16 +4,17 @@ module imm_gen
     input   [size - 1 : 0]   instruction,
     output  reg [size - 1 : 0]   out
 );
-
-reg [6:0]  opcode;
 reg [19:0] sign_extention;
 
-reg [3:0]  alu_option;
+wire [6:0]  opcode;
+wire [3:0]  alu_option;
+
+
+assign opcode       = instruction[6:0];
+assign alu_option   = {opcode[6:4], opcode[2]};
 
 always_comb begin
-    sign_extention = 0;
-    opcode = instruction[6:0];
-    alu_option = {opcode[6:4], opcode[2]};
+    sign_extention  = 0;
 
     case(alu_option)
         // Type I
@@ -24,7 +25,7 @@ always_comb begin
             out[11:0]   = instruction[31:20];
         end 
         // Type S 
-        4'b1100: begin 
+        4'b1000: begin 
             sign_extention = {20{instruction[31]}};
 
             out[31:12]  = sign_extention;
@@ -40,8 +41,7 @@ always_comb begin
             out[11]     = instruction[7];
             out[10:5]   = instruction[30:25];
             out[4:1]    = instruction[11:8];
-            out[0]      = 0;
-
+            out[0]      = 1'b0;
         end
 
     default: out = 0; 
