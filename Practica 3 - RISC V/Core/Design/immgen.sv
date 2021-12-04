eb1a6a84cef65d926fac2a973e8f2052fa6bd357
Javier <presmanes3@gmail.com>
2021-12-04 16:20:35 +0100

    Solved immediate generator bug

--- Practica 3 - RISC V/Core/Design/immgen.sv
+++ Practica 3 - RISC V/Core/Design/immgen.sv
@@ -5,12 +5,13 @@ module imm_gen
     output  reg [size - 1 : 0]   out
 );
 
-reg [6:0] opcode;
-reg sign_extention;
+reg [6:0]  opcode;
+reg [19:0] sign_extention;
 
-reg [3:0] alu_option;
+reg [3:0]  alu_option;
 
 always_comb begin
+    sign_extention = 0;
     opcode = instruction[6:0];
     alu_option = {opcode[6:4], opcode[2]};
 
@@ -20,23 +21,22 @@ always_comb begin
             sign_extention = {20{instruction[31]}};
 
             out[31:12]  = sign_extention; 
-            out[11:0]   = instruction[30:20];
+            out[11:0]   = instruction[31:20];
         end 
         // Type S 
         4'b1100: begin 
             sign_extention = {20{instruction[31]}};
 
             out[31:12]  = sign_extention;
-            out[11:5]   = instruction[30:25];
+            out[11:5]   = instruction[31:25];
             out[4:0]    = instruction[11:7];
         end
         // Type SB
         4'b1100: begin 
             
-            sign_extention = {19{instruction[31]}};
+            sign_extention = {20{instruction[31]}};
 
-            out[31:13]  = sign_extention;
-            out[12]     = instruction[31];
+            out[31:12]  = sign_extention;
             out[11]     = instruction[7];
             out[10:5]   = instruction[30:25];
             out[4:1]    = instruction[11:8];
